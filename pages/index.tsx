316404cf89a9b7d4664fdc1721ff67582c572f87
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-06-02 12:05:21 +0300

    update graphql schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import {gql, useQuery} from "@apollo/client"
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { gql, useQuery } from '@apollo/client';
+import ProductList from '@/components/ProductList';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-const GET_SHOP = gql`
+const GET_PRODUCTS = gql`
   query {
-    findShopByID(id: "366387046774735052") {
-      _id
-      description
-      name
-      ownerID
-      products {
+    getAllProducts {
+      data {
         _id
+        name
+        description
+        price
+		imageUrl
+        shop {
+          _id
+        }
       }
     }
   }
-`;
-
+`
 
 export default function Home() {
-
-	const {data} = useQuery(GET_SHOP);
-	console.log(data)
+  const { data } = useQuery(GET_PRODUCTS);
+  console.log(data.getAllProducts.data);
   return (
-    <main
-    >
-     Home
+    <main className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <ProductList products={data.getAllProducts.data} />
     </main>
-  )
+  );
 }
