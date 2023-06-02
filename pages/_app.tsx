f51f599e8c53db96c149636adc6732c85a25cf7d
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-06-02 05:58:53 +0300

    feat: query data with graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import Nav from '@/components/Nav';
+import { client } from '@/gqlClient';
 import '@/styles/globals.css';
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Nav />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Nav />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {gql, useQuery} from "@apollo/client"
 
 const inter = Inter({ subsets: ['latin'] })
 
+const GET_SHOP = gql`
+  query {
+    findShopByID(id: "366387046774735052") {
+      _id
+      description
+      name
+      ownerID
+      products {
+        _id
+      }
+    }
+  }
+`;
+
+
 export default function Home() {
+
+	const {data} = useQuery(GET_SHOP);
+	console.log(data)
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
      Home
     </main>
